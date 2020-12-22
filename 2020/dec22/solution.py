ad5fe2b0a5e173f322977d3b0466ec4b6e4ff308
Mads <mads@Madss-MBP.localdomain>
2020-12-22 13:02:15 +0100

    added solution to 2020/dec22

--- 2020/dec22/solution.py
+++ 2020/dec22/solution.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10000)
+
 with open("input") as f:
     tmp = f.read().split("\n\n")
     p1, p2 = [list(map(int, p.strip().split('\n')[1:])) for p in tmp]
@@ -21,35 +24,35 @@ def play(p1, p2):
             return points(p1)
 
 def play_rec(p1, p2, history):
+    state = (tuple(p1), tuple(p2))
+    if state in history:
+        return 1, points(p1), history
+    history.add(state)
+
     if len(p1) == 0:
-        return 2, points(p2), history
+         return 2, points(p2), history
     if len(p2) == 0:
-        return 1, points(p1), history
-    if ','.join(map(str, p1 + p2)) in history:
-        return 1, points(p1), history
+         return 1, points(p1), history
 
     c1, c2 = p1.pop(0), p2.pop(0)
-    history += [','.join(map(str, p1 + p2))]
 
     if (len(p1) >= c1 and len(p2) >= c2):
-        winner, score, _ = play_rec(p1.copy()[:c1], p2.copy()[:c2], [])
+        winner, _, _ = play_rec(p1[:c1], p2[:c2], set())
         if winner == 1:
-            winner, score, history = play_rec(p1 + [c1, c2], p2, history)
+            p1 += [c1, c2]
         else:
-            winner, score, history = play_rec(p1, p2 + [c2, c1], history)
+            p2 += [c2, c1]
     else:
         if c1 > c2:
-            winner, score, history = play_rec(p1 + [c1, c2], p2, history)
+            p1 += [c1, c2]
         else:
-            winner, score, history = play_rec(p1, p2 + [c2, c1], history)
-    return winner, score, history
-
+            p2 += [c2, c1]
+    return play_rec(p1, p2, history)
 
 
 # part I
 print(play(p1.copy(), p2.copy()))
 
 # part II
-winner, score, history = play_rec(p1, p2, [])
+winner, score, history = play_rec(p1.copy(), p2.copy(), set())
 print(score)
-## 17922 too low
