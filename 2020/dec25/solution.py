0d3b87dcff21b7574d21fc4a920964e7034f83aa
Mads <mads@Madss-MBP.localdomain>
2020-12-28 21:10:18 +0100

    added solution to 2020/dec25

---
+++ 2020/dec25/solution.py
@@ -0,0 +1,13 @@
+with open("input") as f:
+    keys = [int(l.strip()) for l in f.readlines()]
+
+sub = 7
+p = 20201227
+n = 0
+while True:
+    if pow(sub, n, p) in keys:
+       loop_size = n
+       break
+    n += 1
+
+print(pow(keys[1], loop_size, p))
