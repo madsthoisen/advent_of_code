19e4617446c8b6dc8a43687e7dbaca2eb23415d1
Mads Thøisen <madsthoisen@Madss-MacBook-Pro.local>
2021-12-06 08:31:59 +0100

    Add solution to 2021/dec06

--- 2021/dec06/solution.py
+++ 2021/dec06/solution.py
@@ -5,24 +5,16 @@ with open("input") as f:
     numbers = list(map(int, f.read().strip().split(',')))
 
 
-def new_day(dic):
-    new_dic = defaultdict(int)
-    for k, v in dic.items():
-        if k == 0:
-            new_dic[8] = v
-            new_dic[6] += v
-        else:
-            new_dic[k - 1] += v
-    return new_dic
-
-
-fish = {i: numbers.count(i) for i in range(7)}
+fish = [numbers.count(i) for i in range(9)]
+sizes = {}
 for i in range(256):
-    fish = new_day(fish)
-    if i == 79:
-        # part I
-        print(sum(fish.values()))
+    birth = fish.pop(0)
+    fish[6] += birth
+    fish.append(birth)
+    sizes[i + 1] = sum(fish)
+
+# part I
+print(sizes[80])
 
 # part II
-print(sum(fish.values()))
-
+print(sizes[256])
