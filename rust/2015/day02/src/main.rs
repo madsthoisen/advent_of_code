9789ccfdf1608d3518dd4a9cc5460a1db6fe6c75
Mads Thøisen <madsthoisen@users.noreply.github.com>
2022-10-05 16:14:22 +0200

    Format with rustfmt

--- rust/2015/day02/src/main.rs
+++ rust/2015/day02/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 fn calculate_wrapping_paper(s: [i32; 3]) -> i32 {
     let mut output: i32 = 0;
-    let areas: [i32; 3] = [s[0]*s[1], s[1]*s[2], s[0]*s[2]];
+    let areas: [i32; 3] = [s[0] * s[1], s[1] * s[2], s[0] * s[2]];
     match areas.iter().min() {
         Some(x) => output += *x,
         None => println!("no min found for {:?}", areas),
@@ -14,17 +14,15 @@ fn calculate_wrapping_paper(s: [i32; 3]) -> i32 {
 fn calculate_ribbon(s: [i32; 3]) -> i32 {
     let mut cs = s;
     cs.sort();
-    2*(cs[0] + cs[1]) + cs[0]*cs[1]*cs[2]
+    2 * (cs[0] + cs[1]) + cs[0] * cs[1] * cs[2]
 }
-
-
 
 fn go_through_gifts(s: &str) -> (i32, i32) {
     let mut part_1: i32 = 0;
     let mut part_2: i32 = 0;
-    for line in s.split("\n"){
+    for line in s.split("\n") {
         let mut sides: [i32; 3] = [0; 3];
-        for item in line.split('x').enumerate(){
+        for item in line.split('x').enumerate() {
             let (i, x): (usize, &str) = item;
             let val: i32 = x.parse().unwrap();
             sides[i] = val;
