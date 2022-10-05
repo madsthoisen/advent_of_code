e3ad09fda16b4686ba028a8f4760858083f01bb1
Mads Thøisen <madsthoisen@users.noreply.github.com>
2022-10-05 16:15:44 +0200

    Format with rustfmt

--- rust/2015/day01/src/main.rs
+++ rust/2015/day01/src/main.rs
@@ -1,25 +1,24 @@
 use std::fs;
 
-fn solve(s: &str) -> (i64, i64){
+fn solve(s: &str) -> (i64, i64) {
     let mut count: i64 = 0;
     let mut position: i64 = 1;
     let mut part_2: bool = false;
     for x in s.chars() {
         if x == '(' {
             count += 1;
-            }
-        else if x == ')' {
+        } else if x == ')' {
             count -= 1
-            }
-        if count == -1 && !part_2{
-            part_2 = true;
-            }
-        if !part_2{
-            position += 1;
-            }
         }
-    (count, position)
+        if count == -1 && !part_2 {
+            part_2 = true;
+        }
+        if !part_2 {
+            position += 1;
+        }
     }
+    (count, position)
+}
 
 fn main() {
     let s = fs::read_to_string("../input").expect("");
