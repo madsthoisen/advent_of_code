1de7d7da6d2c8a34d1c69b3fcbd755191f334044
Mads Thøisen <madsthoisen@users.noreply.github.com>
2022-10-19 18:38:03 +0200

    Add solution to 2019/dec11

---
+++ 2019/dec11/solution.py
@@ -0,0 +1,103 @@
+from collections import defaultdict
+from itertools import permutations
+
+
+program = defaultdict(int)
+with open("input") as f:
+    for i, inst in enumerate(f.read().split(",")):
+        program[i] = int(inst)
+
+
+def run(p):
+    i = 0
+    base = 0
+    while True:
+        inst = str(p[i]).zfill(5)
+        opcode = int(inst[3:])
+
+        if inst[2] == "1":
+            a = i + 1
+        else:
+            a = p[i + 1] if inst[2] == "0" else p[i + 1] + base
+        if inst[1] == "1":
+            b = i + 2
+        else:
+            b = p[i + 2] if inst[1] == "0" else p[i + 2] + base
+        if inst[0] == "1":
+            c = i + 3
+        else:
+            c = p[i + 3] if inst[0] == "0" else p[i + 3] + base
+
+        if opcode == 1:
+            p[c] = p[a] + p[b]
+            i += 4
+        elif opcode == 2:
+            p[c] = p[a] * p[b]
+            i += 4
+        elif opcode == 3:
+            p[a] = yield
+            i += 2
+        elif opcode == 4:
+            yield p[a]
+            i += 2
+        elif opcode == 5:
+            i = p[b] if p[a] else i + 3
+        elif opcode == 6:
+            i = p[b] if not p[a] else i + 3
+        elif opcode == 7:
+            p[c] = int(p[a] < p[b])
+            i += 4
+        elif opcode == 8:
+            p[c] = int(p[a] == p[b])
+            i += 4
+        elif opcode == 9:
+            base += p[a]
+            i += 2
+        if opcode == 99:
+            return
+
+
+def get_painting(start_color, program):
+    painting = defaultdict(int)
+    prog = run(program.copy())
+    d = 1j
+    next(prog)
+    pos = 0
+    paint = prog.send(start_color)
+    running = True
+    while running:
+        while True:
+            try:
+                turn = prog.send(painting[pos])
+            except StopIteration:
+                return painting
+            if turn is not None:
+                break
+        painting[pos] = paint
+
+        d = d * 1j if turn == 1 else d / 1j
+        pos += d
+
+        while True:
+            try:
+                paint = prog.send(painting[pos])
+            except StopIteration:
+                return painting
+            if paint is not None:
+                break
+
+
+# part I:
+painting = get_painting(0, program)
+print(len(painting))
+
+# part I
+painting = get_painting(1, program)
+reals = set(int(z.real) for z in painting)
+imags = set(int(z.imag) for z in painting)
+print(
+    "\n".join(
+        "".join("#" if painting[x + y * 1j] else " " for x in range(max(reals), min(reals) - 1, -1))
+        for y in range(max(imags), min(imags) - 1, -1)
+    )
+)
