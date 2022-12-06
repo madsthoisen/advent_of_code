252fe3a72894698f1a38833297c3d728b401f365
madsthoisen <madsthoisen@github.com>
2022-12-06 06:12:38 +0100

    Add solution to 2022/dec06

---
+++ 2022/dec06/solution.py
@@ -0,0 +1,9 @@
+with open("input") as f:
+    s = list(f.read())
+
+
+# part I
+print(min(i for i in range(len(s)) if len(set(s[i - 4: i])) == 4))
+
+# part II
+print(min(i for i in range(len(s)) if len(set(s[i - 14: i])) == 14))
