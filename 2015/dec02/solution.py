888d891fb2eb039ac23a1c2473a7ae48604123ba
madsthoisen <madsthoisen@github.com>
2023-09-09 10:16:59 +0200

    Add solution to 2015/dec02

--- 2015/dec02/solution.py
+++ 2015/dec02/solution.py
@@ -1,17 +1,19 @@
 with open("input") as f:
-    tmp = [line.strip() for line in f.readlines()]
-    gifts = [list(map(int, l.split('x'))) for l in tmp]
+    dim = [list(map(int, x.strip().split('x'))) for x in f.readlines()]
 
-def paper(gift):
-    l, w, h = gift 
-    return 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
 
-def ribbon(gift):
-    l, w, h = gift 
-    return l*w*h + 2*(l + w + h - max([l, w, h]))
+def paper(d):
+    l, w, h = d
+    return 2*l*w + 2*w*h + 2*h*l + min([l*w, w*h, h*l])
+
+
+def ribbon(d):
+    l, w, h = d
+    return 2*(sum(d) - max(d)) + l*w*h
+
 
 # part I
-print(sum(map(paper, gifts)))
+print(sum(map(paper, dim)))
 
 # part II
-print(sum(map(ribbon, gifts)))
+print(sum(map(ribbon, dim)))
