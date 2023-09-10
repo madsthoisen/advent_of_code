3b8962dcddb05da59de0b1e63cf0791c1f319dbe
madsthoisen <madsthoisen@github.com>
2023-09-10 21:21:28 +0200

    Add solution to 2015/dec08

--- 2015/dec08/solution.py
+++ 2015/dec08/solution.py
@@ -8,12 +8,12 @@ patterns = [r"\\x[0-9a-fA-F]{2}", r"\\\\", r"\\\""]
 
 
 def ev(line, part2=False):
-    l = len(line)
+    start = len(line)
     d = {0: 5, 1: 4, 2: 4}
     for i, p in enumerate(patterns):
         sub = ' ' * d[i] if part2 else ' '
         line = re.sub(p, sub, line)
-    return len(line) - l + 4 if part2 else l - len(line) + 2
+    return len(line) - start + 4 if part2 else start - len(line) + 2
 
 
 # part I
