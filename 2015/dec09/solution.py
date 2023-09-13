879c1d8e8763630bf9bce245f9757c59fa7cabcc
madsthoisen <madsthoisen@github.com>
2023-09-13 18:55:11 +0200

    Add solution to 2015/dec09

--- 2015/dec09/solution.py
+++ 2015/dec09/solution.py
@@ -1,40 +1,26 @@
 from collections import defaultdict
-from math import inf
 
 
 with open("input") as f:
-    distances = [line.strip() for line in f.readlines()]
+    distances = [line.strip().split(' ') for line in f.readlines()]
 
 
 G = defaultdict(set)
-vertices = set()
 for line in distances:
-    a, _, b, _, d = line.split(' ')
+    a, _, b, _, d = line
     G[a].add((b, int(d)))
     G[b].add((a, int(d)))
-    vertices = vertices.union({a, b})
+vertices = set(G)
 
 
-def find_shortest_hamiltonian(start, v, G):
-    if len(v) == 0:
+def extreme_hamiltonian(start, not_visited, mode):
+    if len(not_visited) == 0:
         return 0
-    m = inf
-    for e, w in G[start]:
-        if e in v:
-            m = min(m, w + find_shortest_hamiltonian(e, v.difference({e}), G))
-    return m
+    return mode(w + extreme_hamiltonian(e, not_visited - {e}, mode) for e, w in G[start] if e in not_visited)
 
-def find_longest_hamiltonian(start, v, G):
-    if len(v) == 0:
-        return 0
-    m = 0
-    for e, w in G[start]:
-        if e in v:
-            m = max(m, w + find_longest_hamiltonian(e, v.difference({e}), G))
-    return m
 
 # part I
-print(min(find_shortest_hamiltonian(start, vertices.difference({start}), G) for start in vertices))
+print(min(extreme_hamiltonian(start, vertices - {start}, min) for start in vertices))
 
 # part II
-print(max(find_longest_hamiltonian(start, vertices.difference({start}), G) for start in vertices))
+print(max(extreme_hamiltonian(start, vertices - {start}, max) for start in vertices))
