89a80bea14c7d098caad55768495abd740bb31ec
madsthoisen <madsthoisen@github.com>
2023-09-15 23:11:33 +0200

    Add solution to 2015/dec14

--- 2015/dec14/solution.py
+++ 2015/dec14/solution.py
@@ -1,27 +1,24 @@
-import re
-
 with open("input") as f:
-    tmp = [l.strip().split(" can fly ") for l in f.readlines()]
-    specs = {el[0]: list(map(int, re.findall(r"\d+", el[1]))) for el in tmp}
+    tmp = [line.strip().split(' ') for line in f.readlines()]
+    specs = {x[0]: [int(x[3]), int(x[6]), int(x[13])] for x in tmp}  # flight speed, flight time, rest time
 
-reindeers = {k: [0, s[1], 0, 0] for k, s in specs.items()}
+reindeers = {k: [0, s[1], 0, 0] for k, s in specs.items()}  # total travelled, flight time left, time rested, points
 
-T = 2503
-for t in range(T):
-    for r, status in reindeers.items():
-       if status[2] > 0:
-           status[2] -= 1
-       else:
-            status[1] -= 1
-            status[0] += specs[r][0]
-       if status[1] == 0:
-           status[1:3] = specs[r][1:3]
-    for r in reindeers:
-        if reindeers[r][0] == max(r[0] for r in reindeers.values()):
-            reindeers[r][3] += 1
+for _ in range(2503):
+    for k, v in reindeers.items():
+        if v[2] > 0:
+            v[2] -= 1
+        else:
+            v[1] -= 1
+            v[0] += specs[k][0]
+        if v[1] == 0:
+            v[1:3] = specs[k][1:3]
+    for k, v in reindeers.items():
+        v[3] += v[0] == max(x[0] for x in reindeers.values())
+
 
 # part I
-print(max(r[0] for r in reindeers.values()))
+print(max(v[0] for v in reindeers.values()))
 
-# part II
-print(max(r[3] for r in reindeers.values()))
+# part I
+print(max(v[3] for v in reindeers.values()))
