9d5141daa262cd2ddb1d44a81017e8ef9f3b7c5b
madsthoisen <madsthoisen@github.com>
2023-09-16 00:25:26 +0200

    Add solution to 2015/dec15

--- 2015/dec15/solution.py
+++ 2015/dec15/solution.py
@@ -1,33 +1,28 @@
 import re
-
 import numpy as np
 
+
 with open("input") as f:
-    tmp = [l.strip().split(": ") for l in f.readlines()]
-    specs = np.array(list(np.array(list(map(int, re.findall(r"\-?\d+", v)))) for k, v
-            in tmp))
-    cals = specs[:, -1]
-    specs = np.matrix(specs).transpose()[:-1, :]
+    tmp = [line.strip() for line in f.readlines()]
+    ing = np.transpose(np.array([np.array(list(map(int, re.findall(r"-?\d+", x)))) for x in tmp]))
 
 
-def score(use):
-    totals = np.matmul(specs, use.transpose())
-    return np.prod(totals[totals > 0])
+def score_and_calories(dist):
+    v = ing * dist
+    p = v[:-1].sum(axis=1)
+    return np.prod(p[p > 0]), v[-1].sum()
 
 
-combs = (np.array([i, j, k, 100 - i - j - k]) for i in range(101)
-                                              for j in range(101 - i)
-                                              for k in range(101 - i - j))
-
-M, M_cal = 0, 0
-for use in combs:
-    s = score(use)
-    M = max(M, s)
-    if np.dot(use, cals) == 500:
-        M_cal = max(M_cal, s)
+dists = (np.array([i, j, k, 100 - i - j - k]) for i in range(101) for j in range(101 - i) for k in range(101 - i - j))
+p1, p2 = 0, 0
+for dist in dists:
+    s, c = score_and_calories(dist)
+    p1 = max(p1, s)
+    if c == 500:
+        p2 = max(p2, s)
 
 # part I
-print(M)
+print(p1)
 
 # part II
-print(M_cal)
+print(p2)
