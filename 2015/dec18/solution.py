92e4d817f37bc8f4e858d00681ef69a050ecfa60
madsthoisen <madsthoisen@github.com>
2023-09-16 11:15:25 +0200

    Add solution to 2015/dec18

--- 2015/dec18/solution.py
+++ 2015/dec18/solution.py
@@ -1,42 +1,32 @@
-from copy import deepcopy
+import numpy as np
+
 
 with open("input") as f:
-    grid = [list(line.strip()) for line in f.readlines()]
+    lights = np.array([np.array([int(x == '#') for x in line.strip()]) for line in f.readlines()])
 
-inc = [(x, y) for x in [-1, 0, 1] for y in [-1, 0, 1] if (x, y) != (0, 0)]
-h, w = len(grid), len(grid[0])
 
-def neighbors_on(grid, r, c):
-    add = 0
-    for x, y in inc:
-        if 0 <= r + x < h and 0 <= c + y < w: 
-            if grid[r + x][c + y] == '#':
-                add += 1
-    return add 
+def step(lights, part2=False):
+    fixed = lights.copy()
+    for r in range(h):
+        for c in range(w):
+            if part2 and r in {0, h - 1} and c in {0, w - 1}:
+                continue
+            s = fixed[max(r - 1, 0): min(r + 2, h + 1), max(c - 1, 0): min(c + 2, w + 1)].sum()
+            if fixed[r, c] and s not in {3, 4}:
+                lights[r, c] = 0
+            elif not fixed[r, c] and s == 3:
+                lights[r, c] = 1
+    return lights
 
-def round(grid, part2=False):
-    new_grid = deepcopy(grid)
-    for i in range(h):
-        for j in range(w):
-            n = neighbors_on(grid, i, j)
-            if grid[i][j] == '#' and n not in {2, 3}:
-                new_grid[i][j] = '.'
-            if grid[i][j] == '.' and n == 3:
-                new_grid[i][j] = '#'
-    if part2:
-        new_grid[0][0] = new_grid[0][w - 1] = '#'
-        new_grid[h - 1][0] = new_grid[h - 1][w - 1] = '#'
-    return new_grid
+
+w, h = lights.shape
 
 # part I
-grid_1 = deepcopy(grid)
-for _ in range(100):
-    grid_1 = round(grid_1)
-print(sum(r.count('#') for r in grid_1))
+l_tmp = lights.copy()
+[l_tmp := step(l_tmp) for _ in range(100)]
+print(l_tmp.sum())
 
 # part II
-grid[0][0] = grid[0][w - 1] = '#'
-grid[h - 1][0] = grid[h - 1][w - 1] = '#'
-for _ in range(100):
-    grid = round(grid, part2=True)
-print(sum(r.count('#') for r in grid))
+lights[0, h - 1] = lights[0, 0] = lights[w - 1, 0] = lights[w - 1, h - 1] = 1
+[lights := step(lights, True) for _ in range(100)]
+print(lights.sum())
