b94509f5da3f03dd2a4cda0b5d97303c3a68dec2
madsthoisen <madsthoisen@github.com>
2023-09-16 22:21:35 +0200

    Add solution to 2015/dec20

--- 2015/dec20/solution.py
+++ 2015/dec20/solution.py
@@ -1,28 +1,20 @@
+from itertools import count
 from sympy import divisors
 
-with open("input") as f:
-    number = int(f.read())
 
-def part1():
-    i = 0
-    while True:
-        if 10 * sum(divisors(i)) >= number:
-            return i
-        i += 1
+puzzle_input = 33100000
 
-def part2():
-    m = number // 11
-    presents = [0] * m 
-    for elf in range(1, m):
-        for a in range(1, 51):
-            house = elf * a
-            if house >= m:
-                break
-            presents[house] += elf * 11
-    return next(i for i, p in enumerate(presents) if p >= number)
 
 # part I
-print(part1())
+print(next(i for i in count(0) if sum(divisors(i)) * 10 >= puzzle_input))
+
 
 # part II
-print(part2())
+m = puzzle_input // 11
+presents = [0] * m
+for elf in range(1, m):
+    for a in range(1, 51):
+        if (house := elf * a) >= m:
+            break
+        presents[house] += elf * 11
+print(next(i for i, p in enumerate(presents) if p >= puzzle_input))
