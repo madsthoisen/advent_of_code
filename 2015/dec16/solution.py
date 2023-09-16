a4b229a4384ebac77ffb9d30c9048193abf825cd
madsthoisen <madsthoisen@github.com>
2023-09-16 09:46:46 +0200

    Add solution to 2015/dec16

--- 2015/dec16/solution.py
+++ 2015/dec16/solution.py
@@ -1,37 +1,23 @@
-import re
-
 with open("input") as f:
-    tmp = [l.strip() for l in f.readlines()]
-    aunts = [re.split(r": ([a-z])", l) for l in tmp]
-    aunts = {k[4:]: (v1 + v2).split(", ") for k, v1, v2 in aunts}
+    tmp = [line.strip().replace(',', '').replace(':', '').split() for line in f.readlines()]
+    sues = {x[1]: {x[2]: int(x[3]), x[4]: int(x[5]), x[6]: int(x[7])} for x in tmp}
 
-for aunt, att in aunts.items():
-    aunts[aunt] = {el.split(": ")[0]: el.split(": ")[1] for el in att}
+ticker_tape = {"children": 3, "cats": 7, "samoyeds": 2, "pomeranians": 3, "akitas": 0,
+               "vizslas": 0, "goldfish": 5, "trees": 3, "cars": 2, "perfumes": 1}
 
-with open("ticker_tape") as f:
-    tmp = [line.strip() for line in f.readlines()]
-    ticker_tape = {l.split(": ")[0]: l.split(": ")[1] for l in tmp}
+
+def validate(thing, n, part2=False):
+    t = ticker_tape.get(thing)
+    if part2:
+        if thing in {"cats", "trees"}:
+            return n >= t
+        if thing in {"goldfish", "pomerians"}:
+            return n <= t
+    return n == t
+
 
 # part I
-new_aunts = aunts.copy()
-for aunt, att in aunts.items():
-    if any(val != ticker_tape[a] for a, val in att.items()):
-        del new_aunts[aunt]
-
-print(list(new_aunts.keys())[0])
+print(next(sue for sue, things in sues.items() if all(validate(thing, n) for thing, n in things.items())))
 
 # part II
-new_aunts = aunts.copy()
-for aunt, att in aunts.items():
-    for a, val in att.items():
-        if a in {"cats", "trees"}:
-            if val <= ticker_tape[a] and aunt in new_aunts:
-                del new_aunts[aunt]
-        elif a in {"pomerians", "goldfish"}:
-            if val >= ticker_tape[a] and aunt in new_aunts:
-                del new_aunts[aunt]
-        else:
-            if val != ticker_tape[a] and aunt in new_aunts:
-                del new_aunts[aunt]
-
-print(list(new_aunts.keys())[0])
+print(next(sue for sue, things in sues.items() if all(validate(thing, n, True) for thing, n in things.items())))
