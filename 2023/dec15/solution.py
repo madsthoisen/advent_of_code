c99458cce6603a4bd00af697a14a393fb909e2b6
madsthoisen <madsthoisen@github.com>
2023-12-15 20:44:21 +0100

    Add solution to 2023/dec15

--- 2023/dec15/solution.py
+++ 2023/dec15/solution.py
@@ -5,25 +5,21 @@ with open("input") as f:
     ins = f.read().strip().split(',')
 
 
-def get(s):
+def h(s):
     return reduce(lambda x, y: (x + ord(y)) * 17 % 256, s, 0)
 
 
 # part I
-print(sum(get(s) for s in ins))
+print(sum(h(s) for s in ins))
 
 # part II
 boxes = {i: dict() for i in range(256)}
 for x in ins:
     if '-' in x:
         label = x[:-1]
-        box = get(label)
-        boxes[box].pop(label, None)
+        boxes[h(label)].pop(label, None)
     elif '=' in x:
         label, fl = x.split('=')
-        box = get(label)
-        boxes[box][label] = int(fl)
-        for b in set(boxes) - {box}:
-            boxes[b].pop(label, None)
+        boxes[h(label)][label] = int(fl)
 
 print(sum((k + 1) * (i + 1) * b for k, box in boxes.items() for i, (_, b) in enumerate(box.items())))
