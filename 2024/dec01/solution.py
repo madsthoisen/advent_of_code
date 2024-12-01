fbaaa3965e28d40f2a64a2a07a3d0b974081459a
madsthoisen <madsthoisen@github.com>
2024-12-01 06:27:08 +0100

    Add solution to dec 1, 2024

---
+++ 2024/dec01/solution.py
@@ -0,0 +1,13 @@
+with open("input") as f:
+    lines = [list(map(int, line.strip().split())) for line in f.readlines()]
+
+
+la, lb = list(zip(*lines))
+
+
+# Part I
+print(sum(abs(a - b) for a, b in zip(sorted(la), sorted(lb))))
+
+
+# Part II
+print(sum(a * lb.count(a) for a in la))
