b6d7b5f5cd5639837dfce848d624b64a7a34717a
madsthoisen <madsthoisen@github.com>
2024-12-08 11:27:58 +0100

    Add solution to 2024/dec08

--- 2024/dec08/solution.py
+++ 2024/dec08/solution.py
@@ -2,7 +2,7 @@ import string
 
 from collections import defaultdict
 from itertools import count
-from itertools import combinations
+from itertools import permutations
 
 
 with open("input") as f:
@@ -18,24 +18,18 @@ for r, row in enumerate(lines):
 
 def get_antinodes(a, b, part):
     iterator = [2] if part == 1 else count(1)
-    dx, dy = a[0] - b[0], a[1] - b[1]
     out = set()
     for i in iterator:
-        a1 = (a[0] - i * dx, a[1] - i * dy)
-        a2 = (b[0] + i * dx, b[1] + i * dy)
-        a1_inside = (0 <= a1[0] < len(lines) and 0 <= a1[1] < len(lines[0]))
-        a2_inside = (0 <= a2[0] < len(lines) and 0 <= a2[1] < len(lines[0]))
-        if a1_inside:
+        a1 = (a[0] - i * (a[0] - b[0]), a[1] - i * (a[1] - b[1]))
+        if 0 <= a1[0] < len(lines) and 0 <= a1[1] < len(lines[0]):
             out.add(a1)
-        if a2_inside:
-            out.add(a2)
-        if not a1_inside and not a2_inside:
+        else:
             break
     return out
 
 
 # part I
-print(len({p for nodes in freqs.values() for a, b in combinations(nodes, 2) for p in get_antinodes(a, b, part=1)}))
+print(len({p for nodes in freqs.values() for a, b in permutations(nodes, 2) for p in get_antinodes(a, b, part=1)}))
 
 # part II
-print(len({p for nodes in freqs.values() for a, b in combinations(nodes, 2) for p in get_antinodes(a, b, part=2)}))
+print(len({p for nodes in freqs.values() for a, b in permutations(nodes, 2) for p in get_antinodes(a, b, part=2)}))
