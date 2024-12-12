7d920137ceb5aa86d1f2a6b981bae55bb63736a1
madsthoisen <madsthoisen@github.com>
2024-12-12 15:09:23 +0100

    Add solution to 2024/dec12

--- 2024/dec12/solution.py
+++ 2024/dec12/solution.py
@@ -12,7 +12,7 @@ G.add_edges_from([(z, z + dz) for z, val in grid.items() for dz in DIRS if grid.
 
 part1, part2 = 0, 0
 for region in nx.connected_components(G):
-    fence = [(z, z + dz) for z in region for dz in DIRS if grid.get(z + dz) != grid[list(region)[0]]]
+    fence = [(z, z + dz) for z in region for dz in DIRS if z + dz not in region]
     part1 += len(region) * len(fence)
     # remove pieces of fence where there is another piece below or to the right
     remove = {(z, nz) for z, nz in fence for dz in [1j, 1] if (z + dz, nz + dz) in fence}
