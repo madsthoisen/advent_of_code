568bc2396d1f3fbee235c8f19e01118f66b702ab
madsthoisen <madsthoisen@github.com>
2024-12-22 07:10:49 +0100

    Add solution to 2024/dec22

---
+++ 2024/dec22/solution.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+
+with open("input") as f:
+    numbers = [int(line) for line in f.readlines()]
+
+
+def do(n):
+    m = 16777216
+    n = (n * 2**6 ^ n) % m
+    n = (n // 2**5 ^ n) % m
+    return (n * 2**11 ^ n) % m
+
+
+dd = defaultdict(int)
+add = 0
+for num in numbers:
+    vals = [num % 10] + [(num := do(num)) % 10 for _ in range(2000)]
+    changes = [b - a for a, b in zip(vals, vals[1:])]
+    seen = set()
+    for i in range(4, len(changes)):
+        if (t := tuple(changes[i - 4: i])) not in seen:
+            seen.add(t)
+            dd[t] += vals[i]
+    add += num
+
+# part I
+print(add)
+
+# part II
+print(max(dd.values()))
